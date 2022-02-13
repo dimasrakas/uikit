370e5115cfc452b7352080b56ae0e380c240a391
dimasrakas <dimasrakas@gmail.com>
2022-02-13 20:25:16 +0700

    Add MDX format for Button documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
-    "../packages/**/*.stories.@(js|jsx|ts|tsx)",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
+  ],
   framework: "@storybook/vue",
 };
